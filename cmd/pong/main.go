748cbc6dccec1ea476188953cc888972d98a0216
vinibispo <vini.bispo015@gmail.com>
2024-08-17 11:38:28 -0300

    Check for collision between paddles

--- cmd/pong/main.go
+++ cmd/pong/main.go
@@ -23,11 +23,20 @@ func main() {
 	cpu := models.NewCPUPaddle(10, float32((screenHeight-playerHeight)/2), float32(playerWidth), float32(playerHeight), 6)
 	for !rl.WindowShouldClose() {
 		rl.BeginDrawing()
-		rl.ClearBackground(rl.Black)
 
 		ball.Update()
 		player1.Update()
+		cpu.Update(int(ball.Y))
 
+		if rl.CheckCollisionCircleRec(rl.Vector2{ball.X, ball.Y}, float32(ball.Radius), rl.Rectangle{player1.X, player1.Y, player1.Width, player1.Height}) {
+			ball.SpeedX = -ball.SpeedX
+		}
+
+		if rl.CheckCollisionCircleRec(rl.Vector2{ball.X, ball.Y}, float32(ball.Radius), rl.Rectangle{cpu.X, cpu.Y, cpu.Width, cpu.Height}) {
+			ball.SpeedX = -ball.SpeedX
+		}
+
+		rl.ClearBackground(rl.Black)
 		rl.DrawLine(screenWidth/2, 0, screenWidth/2, screenHeight, rl.White)
 		ball.Draw()
 		cpu.Draw()
